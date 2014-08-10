4eaf510b940e86f3eb32c13474b662c578cb824f
Ryex <ryexander@gmail.com>
2014-08-10 06:08:32 +0000

    modifiny test to be run from anyware

--- test/pyitect_test.py
+++ test/pyitect_test.py
@@ -1,8 +1,13 @@
-import pyitect                                                                                                                                                                                                                                         
+import os
+import sys
+folder_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(folder_path))
+
+import pyitect                                                                                                                                                                                                                                   
 import json                                                                                                                                                                                                                                            
-cfgfile = open("config.json")                                                                                                                                                                                                                                            
+cfgfile = open(os.path.join(folder_path, "config.json"))                                                                                                                                                                                                                                            
 cfg = json.load(cfgfile)                                                                                                                                                                                                                                            
 system = pyitect.System(cfg)                                                                                                                                                                                                                                            
-system.search("test/plugins")                                                                                                                                                                                                                                            
+system.search(os.path.join(folder_path, "plugins"))                                                                                                                                                                                                                                            
 bar = system.load("bar")
 bar()
